e66d192ea24bbd7b2d376e929a8f942fa0b87f56
GrantBirki <grant.birkinbine@gmail.com>
2025-04-03 13:08:57 -0700

    benchmark results

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@
 
 source "https://rubygems.org"
 
+gem "faraday", "~> 2.12", ">= 2.12.2"
+gem "faraday-net_http_persistent", "~> 2.3"
+gem "httparty", "~> 0.23.1"
 gem "net-http-persistent", "~> 4.0", ">= 4.0.5"
 gem "puma", "~> 6.6"
 gem "sinatra", "~> 4.1", ">= 4.1.1"
--- benchmarks/http_clients.rb
+++ benchmarks/http_clients.rb
@@ -1,41 +1,27 @@
 # frozen_string_literal: true
 
 # $ bundle exec ruby benchmarks/http_clients.rb
-# Benchmarking Persistent HTTP Client...
-# Request 1000: Response Code: 200
-# Request 2000: Response Code: 200
-# Request 3000: Response Code: 200
-# Request 4000: Response Code: 200
-# Request 5000: Response Code: 200
-# Request 6000: Response Code: 200
-# Request 7000: Response Code: 200
-# Request 8000: Response Code: 200
-# Request 9000: Response Code: 200
-# Request 10000: Response Code: 200
-# Total Time (Persistent HTTP Client): 2052.04 ms
-# Benchmarking Net::HTTP...
-# Request 1000: Response Code: 200
-# Request 2000: Response Code: 200
-# Request 3000: Response Code: 200
-# Request 4000: Response Code: 200
-# Request 5000: Response Code: 200
-# Request 6000: Response Code: 200
-# Request 7000: Response Code: 200
-# Request 8000: Response Code: 200
-# Request 9000: Response Code: 200
-# Request 10000: Response Code: 200
-# Total Time (Net::HTTP): 8005.07 ms
-# Persistent HTTP Client is 3.9x faster than Net::HTTP
+# This script benchmarks multiple HTTP clients for 10,000 requests to a Sinatra server running on localhost.
+# Comparison Results:
+# Persistent HTTP Client is 0.95x faster than Net::HTTP
+# Persistent HTTP Client is 1.18x faster than Faraday
 
 require_relative "../lib/persistent_http_client"
+require "faraday/net_http_persistent"
 require "net/http"
+require "faraday"
+require "httparty"
 require "benchmark"
 
 HOST = "http://127.0.0.1:8080"
+ATTEMPTS = 30_000
 
-ATTEMPTS = 10_000
+# Helper method to print benchmark results
+def print_results(client_name, total_time)
+  puts "Total Time (#{client_name}): #{(total_time * 1000).round(2)} ms"
+end
 
-# Benchmark 10,000 requests using the persistent HTTP client
+# Benchmark Persistent HTTP Client
 def benchmark_persistent_http_client
   client = HTTP::Client.new(HOST)
   total_time = Benchmark.realtime do
@@ -45,27 +31,72 @@ def benchmark_persistent_http_client
     end
   end
   client.close!
-  puts "Total Time (Persistent HTTP Client): #{(total_time * 1000).round(2)} ms"
-  return total_time
+  print_results("Persistent HTTP Client", total_time)
+  total_time
 end
 
-puts "Benchmarking Persistent HTTP Client..."
-persistent_client_time = benchmark_persistent_http_client
-
-# Benchmark 10,000 requests using Net::HTTP with new connections
+# Benchmark Net::HTTP
 def benchmark_net_http
   uri = URI(HOST)
   total_time = Benchmark.realtime do
-    ATTEMPTS.times do |i|
-      response = Net::HTTP.get_response(uri)
-      puts "Request #{i + 1}: Response Code: #{response.code}" if (i + 1) % 1000 == 0
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      ATTEMPTS.times do |i|
+        response = http.get(uri)
+        puts "Request #{i + 1}: Response Code: #{response.code}" if (i + 1) % 1000 == 0
+      end
     end
   end
-  puts "Total Time (Net::HTTP): #{(total_time * 1000).round(2)} ms"
-  return total_time
+  print_results("Net::HTTP", total_time)
+  total_time
 end
+
+# Benchmark Faraday
+def benchmark_faraday
+  connection = Faraday.new(url: HOST) do |faraday|
+    faraday.adapter :net_http_persistent
+  end
+  total_time = Benchmark.realtime do
+    ATTEMPTS.times do |i|
+      response = connection.get("/")
+      puts "Request #{i + 1}: Response Code: #{response.status}" if (i + 1) % 1000 == 0
+    end
+  end
+  print_results("Faraday", total_time)
+  total_time
+end
+
+# does not even support persistent connections
+# # Benchmark HTTParty
+# def benchmark_httparty
+#   total_time = Benchmark.realtime do
+#     ATTEMPTS.times do |i|
+#       response = HTTParty.get(HOST)
+#       puts "Request #{i + 1}: Response Code: #{response.code}" if (i + 1) % 1000 == 0
+#     end
+#   end
+#   print_results("HTTParty", total_time)
+#   total_time
+# end
+
+# Run all benchmarks
+puts "Benchmarking Faraday..."
+faraday_time = benchmark_faraday
+sleep 2
+
+puts "Benchmarking Persistent HTTP Client..."
+persistent_client_time = benchmark_persistent_http_client
+sleep 2
 
 puts "Benchmarking Net::HTTP..."
 net_http_time = benchmark_net_http
+sleep 2
 
+# puts "Benchmarking HTTParty..."
+# httparty_time = benchmark_httparty
+# sleep 2
+
+# Compare results
+puts "\nComparison Results:"
 puts "Persistent HTTP Client is #{(net_http_time / persistent_client_time).round(2)}x faster than Net::HTTP"
+puts "Persistent HTTP Client is #{(faraday_time / persistent_client_time).round(2)}x faster than Faraday"
+# puts "Persistent HTTP Client is #{(httparty_time / persistent_client_time).round(2)}x faster than HTTParty"
