8ec8e0ec985af0fc2d261e0231f9abae94635a1a
GrantBirki <grant.birkinbine@gmail.com>
2025-04-03 10:12:33 -0700

    set

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,10 @@ bind "tcp://0.0.0.0:8080"
 # single mode: https://github.com/puma/puma/blob/master/docs/deployment.md#single-vs-cluster-mode
 workers 0
 
+# Set keep-alive timeout to 20 seconds
+# https://github.com/puma/puma/blob/b836667e9fde7e982880d28e03da9c0f87085de2/lib/puma/dsl.rb#L347-L358
+persistent_timeout 20
+
 log_formatter do |msg|
   timestamp = Time.now.strftime("%Y-%m-%dT%H:%M:%S.%L%z")
   {
